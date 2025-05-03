d050f711d401275a6a12b220baa994907ee50557
baya <abaya6054@gmail.com>
2025-05-03 12:13:51 +0100

    setup and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+const font  = Nunito({
+  subsets: ["latin"],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={font.className}
       >
         {children}
       </body>
